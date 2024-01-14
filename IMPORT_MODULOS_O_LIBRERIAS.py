c9a452f20e7d24e58df1fd61665b87a78407cea8
salextros <salextros@gmail.com>
2024-01-13 21:07:42 -0500

    Continuacion_modulos_python

--- IMPORT_MODULOS_O_LIBRERIAS.py
+++ IMPORT_MODULOS_O_LIBRERIAS.py
@@ -1,2 +1,54 @@
 import math
+
 print(math.sin(math.pi/2))
+print("")
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+pi = 3.14
+
+print(sin(pi/2))
+print("")
+print(math.sin(math.pi/2))
+
+from math import sin, pi
+print("")
+print(sin(pi/2))
+
+from math import sin, pi
+print("")
+print(sin(pi / 2))
+
+pi = 3.14
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+print("")
+print(sin(pi / 2))
+
+#########################################################################
+pi = 3.14
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+print("")
+print(sin(pi / 2))
+
+from math import sin, pi
+print("")
+print(sin(pi / 2))
